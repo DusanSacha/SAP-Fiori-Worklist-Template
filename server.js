d7e95c2e25d09c155a0294d17403c401e85c7955
DusanSacha <sacha.dusan@gmail.com>
2016-11-08 16:17:49 +0100

    node server

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+//port definition
+app.set('port', (5000));
+
+//server starting
+var server = app.listen(app.get('port'), function() {
+	var port = server.address().port;
+	console.log('Express server listening on port %s', port);
+})
